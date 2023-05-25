d1ba3e8628ed39c3a84d60f933c2d6c0b3813714
lgswin <lgswin@gmail.com>
2023-05-25 12:55:59 +0900

    disney : image banner #2

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import axios from '../api/axios';
 import requests from '../api/requests'
 import React, { useEffect, useState } from 'react'
+import "./Banner.css";
 
 const Banner = () => {
 
@@ -26,8 +27,43 @@ const Banner = () => {
     setMovie(movieDetail)
   }
 
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substring(0, n) + "..." : str;
+  }
+
   return (
-    <div>Banner</div>
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover"
+      }}
+    >
+      <div className='banner__contents'>
+        <h1 className='banner__title'>
+          {movie.title || movie.name || movie.original_name}
+        </h1>
+
+        <div className='banner__buttons'>
+          {movie?.videos?.results[0]?.key && 
+            <button 
+              className='banner_button play'
+            >
+              Play
+            </button>
+          }
+        </div>
+
+        <p className='banner__description'>
+          {truncate(movie.overview, 100)}
+        </p>
+
+      </div>
+      <div className='banner--fadeBottom' />
+
+    </header>
+
   )
 }
 
