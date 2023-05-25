11aa97130bacace2f52e1e7efa1a53a356dce01a
lgswin <lgswin@gmail.com>
2023-05-25 10:43:26 +0900

    disney : useState, useEffect -> scroll

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import styled from 'styled-components'
 
 // this is test
 const Nav = () => {
-  return (
-    <NavWrapper>
-        <Logo>
-            <img alt="Disney Plus Logo" src="./images/logo.svg" onClick={() =>  (window.location.href="/")} 
-            />
-        </Logo>
-    </NavWrapper>
-  )
+
+    const [show, setShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 50) {
+                setShow(true);
+            } else {
+                setShow(false);
+            }
+        })
+        return () => {
+            window.removeEventListener('scroll', () => {});
+        }
+    }, [])
+
+    return (
+        <NavWrapper show={show}>
+            <Logo>
+                <img alt="Disney Plus Logo" src="./images/logo.svg" onClick={() =>  (window.location.href="/")} />
+            </Logo>
+        </NavWrapper>
+    )
 }
 
 export default Nav
@@ -21,7 +36,7 @@ const NavWrapper = styled.div`
     left: 0;
     right: 0;
     height: 70px;
-    background-color: #090b13;
+    background-color: ${props => props.show ? "#090b13" : "transparent"};
     display: flex;
     justify-content: space-between;
     align-items: center;
